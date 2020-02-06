b25de7eac9930caa0716a47f7f67f4e3b46b3d44
mohamad omar <mohat58@gmail.com>
2020-02-06 10:02:27 +0200

    dcd

--- lab6.js
+++ lab6.js
@@ -5,6 +5,13 @@ var r =prompt("How are you");
 
 confirm("your name");
 confirm("your age");
-if (w<18){
+while (w <18){
+ if (w<18){
     alert("NOT ALLOWED, should be 18+");
+ }
+}
+var r1 =prompt("your fav movies");
+for (i=0;i<10;i++)
+{
+    document.write(r1);
 }
