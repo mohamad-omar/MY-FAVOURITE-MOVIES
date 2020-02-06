01836be15d39433c502f3ae477cb49223952f4f5
mohamad omar <mohat58@gmail.com>
2020-02-06 09:47:37 +0200

    sasadsd

---
+++ lab6.js
@@ -0,0 +1,10 @@
+var m =prompt("input name");
+var w =prompt("input age");
+var r =prompt("How are you");
+
+
+confirm("your name");
+confirm("your age");
+if (w<18){
+    alert("NOT ALLOWED, should be 18+");
+}
